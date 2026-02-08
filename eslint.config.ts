c18e05dfd96e15f56a25eb685e1d9802be934f5a
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-08 19:50:22 +0530

    Added ESLint configuration file

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+  tseslint.configs.recommended,
+]);
