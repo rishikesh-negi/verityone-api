1a1acc97ea1f7d31008b7cdfa83039b6db70a29b
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-09 20:01:32 +0530

    Removed the old defineConfig function to create a flat config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,25 @@
 import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import eslintPluginImport from "eslint-plugin-import";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginImport.flatConfigs.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
     },
   },
-  tseslint.configs.recommended,
-]);
+  {
+    files: ["eslint.config.*"],
+    rules: {
+      "import/no-unresolved": "off",
+    },
+  },
+];
