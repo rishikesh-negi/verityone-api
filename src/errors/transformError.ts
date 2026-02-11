0fd2c7b3f8db5f0d4ff8890e7f41eefe2c106023
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-11 19:48:58 +0530

    Moved file to the 'src/errors' folder. Fixed parameter types and error transformation logic

---
+++ src/errors/transformError.ts
@@ -0,0 +1,36 @@
+import { MongoServerError } from "mongodb";
+import mongoose from "mongoose";
+
+import { AppError, BadRequestError, NotFoundError } from "./AppError.js";
+
+export const transformError = (err: Error): Error => {
+  if (err instanceof mongoose.Error.CastError)
+    return new BadRequestError(`Invalid ${err.path}: ${err.value}`);
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    const messages = Object.values(err.errors).map((er: Error) => er.message);
+    return new BadRequestError(`Invalid input data. ${messages.join(", ")}`);
+  }
+
+  if (err instanceof mongoose.Error.DocumentNotFoundError)
+    return new NotFoundError("No such document found");
+
+  // Common MongoDB errors:
+  if (err instanceof MongoServerError) {
+    // DuplicateKeyError:
+    if (err.code === 11000) {
+      const field = Object.keys(err["keyValue"])[0]!;
+      const value = err["keyValue"][field];
+      const message = `Unique value expteced: '${value}' already exists in '${field}'`;
+      return new AppError(message, 409);
+    }
+  }
+
+  if (err.name === "JsonWebTokenError")
+    return new AppError("Invalid token. Please log in again", 401);
+
+  if (err.name === "TokenExpiredError")
+    return new AppError("Session expired. Please log in again", 401);
+
+  return err;
+};
