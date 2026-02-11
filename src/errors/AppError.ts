915f49a174e55510bf135bcf4740f426a67d7775
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-11 19:48:04 +0530

    Moved file to the 'src/errors' folder

---
+++ src/errors/AppError.ts
@@ -0,0 +1,41 @@
+export class AppError extends Error {
+  public statusCode: number;
+  public status: "fail" | "error";
+  public readonly isOperational: true;
+
+  constructor(message: string, statusCode: number = 500) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedAccessError extends AppError {
+  constructor(
+    message = "Authentication/Authorization Required for this Action",
+  ) {
+    super(message, 401);
+  }
+}
+
+export class ForbiddebAccessError extends AppError {
+  constructor(message = "Authorization Failed") {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = "Resource Not Found") {
+    super(message, 404);
+  }
+}
