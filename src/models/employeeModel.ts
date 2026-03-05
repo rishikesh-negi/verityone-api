b3946892776236d5759d635f02c424bf7a6a58f0
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-05 20:18:58 +0530

    Added schema definitions for Employee document properties

--- src/models/employeeModel.ts
+++ src/models/employeeModel.ts
@@ -1,70 +1,110 @@
 import { Schema, model, type InferSchemaType } from "mongoose";
 import {
+  hashPasswordPreSave,
+  setPasswordChangeTimestampPreSave,
+} from "../middleware/passwordManagementMiddleware.js";
+import {
+  emailAddressFormatValidator,
   partialNameValidator,
   passwordValidator,
   usernameValidator,
 } from "../utils/stringValidators.js";
 
-const employeeSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, "First name is required"],
-    trim: true,
-    minlength: [2, "Invalid first name"],
-    maxlength: [40, "First name must not exceed 40 characters"],
-    validate: {
-      validator: partialNameValidator,
-      message: "Please enter a valid first name",
+const employeeSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      minlength: [2, "Invalid first name"],
+      maxlength: [40, "First name must not exceed 40 characters"],
+      validate: {
+        validator: partialNameValidator,
+        message: "Please enter a valid first name",
+      },
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+      minlength: [2, "Invalid last name"],
+      maxlength: [40, "Last name must not exceed 40 characters"],
+      validate: {
+        validator: partialNameValidator,
+        message: "Please enter a valid last name",
+      },
+    },
+    fullName: {
+      type: String,
+      default: function (): string {
+        return `${this.firstName} ${this.lastName}`;
+      },
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      unique: [true, "This username is taken. Try a different one"],
+      minlength: [3, "Username must have at least 3 characters"],
+      maxlength: [25, "Username length cannot exceed 25 characters"],
+      validate: {
+        validator: usernameValidator,
+        message:
+          "Only letters, numbers, and underscores allowed. Must contain at least one letter",
+      },
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "A valid email address is required"],
+      unique: [true, "An account with this email address already exists"],
+      lowercase: true,
+      validate: {
+        validator: emailAddressFormatValidator,
+        message: "Invalid email address format",
+      },
+      maxLength: [50, "The email address cannot exceed 50 characters"],
+      message: "Please provide a valid email address",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required to secure your account"],
+      select: false,
+      validate: {
+        validator: passwordValidator,
+        message:
+          "Password must contain minimum 8 characters, uppercase and lowercase letters, a number, and a special character",
+      },
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    createdAt: {
+      type: Date,
+      default: new Date(Date.now()),
+      select: false,
+      immutable: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
     },
   },
-  lastName: {
-    type: String,
-    required: [true, "Last name is required"],
-    trim: true,
-    minlength: [2, "Invalid last name"],
-    maxlength: [40, "Last name must not exceed 40 characters"],
-    validate: {
-      validator: partialNameValidator,
-      message: "Please enter a valid last name",
-    },
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    trim: true,
-    unique: [true, "This username is taken. Try a different one"],
-    minlength: [3, "Username must have at least 3 characters"],
-    maxlength: [25, "Username length cannot exceed 25 characters"],
-    validate: {
-      validator: usernameValidator,
-      message:
-        "Only letters, numbers, and underscores allowed. Must contain at least one letter",
-    },
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required to secure your account"],
-    select: false,
-    validate: {
-      validator: passwordValidator,
-      message:
-        "Password must contain minimum 8 characters, uppercase and lowercase letter/s, a number, and a special character",
-    },
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-    immutable: true,
-  },
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
+  { timestamps: true },
+);
+
+employeeSchema.pre("save", function () {
+  hashPasswordPreSave(this);
+});
+
+employeeSchema.pre("save", function () {
+  setPasswordChangeTimestampPreSave(this);
+});
+
+employeeSchema.pre(/^find/, async function () {
+  if (!("find" in this)) return;
+  this.find({ active: { $ne: false } });
 });
 
 export type IEmployee = InferSchemaType<typeof employeeSchema>;
