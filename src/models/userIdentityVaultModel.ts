c8634cca03646e6970fb46fe856749082bc9a0ef
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-05 20:22:15 +0530

    Created a Mongoose model for Vault documents to anonymise Employee identity

---
+++ src/models/userIdentityVaultModel.ts
@@ -0,0 +1,34 @@
+import { model, Schema, type InferSchemaType } from "mongoose";
+
+const userIdentityVaultSchema = new Schema({
+  userId: {
+    type: Schema.ObjectId,
+    ref: "Employee",
+    required: [true, "User ID is required for creating an anonymous identity"],
+  },
+  anonymousId: {
+    type: Schema.Types.UUID,
+    requried: [
+      true,
+      "A UUID is required for mapping reviews to the anonymous identity",
+    ],
+  },
+  tokenHash: {
+    type: String,
+    required: [
+      true,
+      "A token is required for tokenized access to the anonymous identity",
+    ],
+  },
+  active: { type: Boolean, default: true, select: false },
+  createdAt: { type: Date, default: Date.now(), select: false },
+});
+
+export type IUserIdentityVault = InferSchemaType<
+  typeof userIdentityVaultSchema
+>;
+
+export const UserIdentityVault = model<IUserIdentityVault>(
+  "UserIdentityVault",
+  userIdentityVaultSchema,
+);
