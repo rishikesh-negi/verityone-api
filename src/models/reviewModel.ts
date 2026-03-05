210e2610c6212afb4998a4751b0bac9f8194b2ec
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-05 20:21:29 +0530

    Created a Mongoose model for Review documents

---
+++ src/models/reviewModel.ts
@@ -0,0 +1,30 @@
+import { model, Schema, type InferSchemaType } from "mongoose";
+
+const reviewSchema = new Schema({
+  anonymousId: {
+    type: Schema.Types.UUID,
+  },
+  organization: {
+    type: Schema.ObjectId,
+    ref: "Organization",
+    required: [true, "A review must be for an organization"],
+  },
+  rating: {
+    type: Number,
+    required: [true, "Please provide a rating"],
+    min: [1, "Rating cannot be below 1"],
+    max: [10, "Rating cannot exceed 10"],
+  },
+  reviewText: {
+    type: String,
+    trim: true,
+    minLength: [20, "Review length should be at least 20 characters"],
+    maxLength: [1000, "Review cannot exceed 1000 characters"],
+  },
+  biased: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now(), select: false },
+});
+
+export type IReview = InferSchemaType<typeof reviewSchema>;
+
+export const Review = model<IReview>("Review", reviewSchema);
