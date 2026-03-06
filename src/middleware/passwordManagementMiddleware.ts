2eb498c9d3f8ad44273fdab86183537d7666b2ac
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-06 19:46:29 +0530

    Explicitly set the 'this' keyword of middleware and methods instead of using higher order functions to return to return them

--- src/middleware/passwordManagementMiddleware.ts
+++ src/middleware/passwordManagementMiddleware.ts
@@ -1,25 +1,23 @@
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 import type { HydratedDocument } from "mongoose";
 import type { IEmployee } from "../models/employeeModel.js";
 import type { IOrganization } from "../models/organizationModel.js";
-import crypto from "crypto";
 
 // The HydratedDocument utility type is used to type a mongoose document:
-export const hashPasswordPreSave = (
-  doc: HydratedDocument<IEmployee | IOrganization>,
-) =>
-  async function (): Promise<void> {
-    if (!doc.isModified("password")) return;
-    doc.password = await bcrypt.hash(doc.password, 12);
-  };
+export async function hashPasswordPreSave(
+  this: HydratedDocument<IEmployee | IOrganization>,
+): Promise<void> {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 12);
+}
 
-export const setPasswordChangeTimestampPreSave = (
-  doc: HydratedDocument<IEmployee | IOrganization>,
-) =>
-  async function (): Promise<void> {
-    if (!doc.isModified("password") || doc.isNew) return;
-    doc.passwordChangedAt = new Date(Date.now() - 1000);
-  };
+export async function setPasswordChangeTimestampPreSave(
+  this: HydratedDocument<IEmployee | IOrganization>,
+): Promise<void> {
+  if (!this.isModified("password") || this.isNew) return;
+  this.passwordChangedAt = new Date(Date.now() - 1000);
+}
 
 export async function matchPasswords(
   candidatePassword: string,
@@ -28,32 +26,30 @@ export async function matchPasswords(
   return await bcrypt.compare(candidatePassword, userPassword);
 }
 
-export const passwordChangedAfter = (
-  doc: HydratedDocument<IEmployee | IOrganization>,
+export function passwordChangedAfter(
+  this: HydratedDocument<IEmployee | IOrganization>,
   JWTTimestamp: number,
-) =>
-  function (): boolean {
-    if (doc.passwordChangedAt) {
-      const passwordChangeTimestamp = Math.floor(
-        doc.passwordChangedAt.getTime() / 1000,
-      );
-      return JWTTimestamp < passwordChangeTimestamp;
-    }
+): boolean {
+  if (this.passwordChangedAt) {
+    const passwordChangeTimestamp = Math.floor(
+      this.passwordChangedAt.getTime() / 1000,
+    );
+    return JWTTimestamp < passwordChangeTimestamp;
+  }
 
-    return false;
-  };
+  return false;
+}
 
-export const createPaswordResetToken = (
-  doc: HydratedDocument<IEmployee | IOrganization>,
-) =>
-  function () {
-    const resetToken = crypto.randomBytes(32).toString("hex");
-    doc.passwordResetToken = crypto
-      .createHash("sha256")
-      .update(resetToken)
-      .digest("hex");
+export function createPaswordResetToken(
+  this: HydratedDocument<IEmployee | IOrganization>,
+): string {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-    doc.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
+  this.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
 
-    return resetToken;
-  };
+  return resetToken;
+}
