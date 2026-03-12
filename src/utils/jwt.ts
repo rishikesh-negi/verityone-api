99d00cd48e5504cb7fa044ef21e561487d36bbad
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-12 20:20:10 +0530

    Assigned env variables to top-level variables of the module, corrected the async function's return type, and completed the function to create an access token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,23 +1,20 @@
 import crypto from "crypto";
 import jwt, { type JwtPayload, type Secret } from "jsonwebtoken";
-import type { HydratedDocument } from "mongoose";
 import ms from "ms";
 import { promisify } from "util";
-import type { IEmployee } from "../models/employeeModel.js";
-import type { IOrganization } from "../models/organizationModel.js";
 import type { AuthJWTPayload } from "../types/types.js";
 
 const ACCESS_JWT_SECRET: string = process.env["ACCESS_JWT_SECRET"]!;
 const REFRESH_JWT_SECRET: string = process.env["REFREST_JWT_SECRET"]!;
 
+const REFRESH_JWT_EXPIRES_IN: string = process.env["REFRESH_JWT_EXPIRES_IN"]!;
+const ACCESS_JWT_EXPIRES_IN: string = process.env["ACCESS_JWT_EXPIRES_IN"]!;
+
 export function signAuthJWT(payload: object, tokenType: "access" | "refresh") {
-  const jwtSecret: Secret =
-    tokenType === "access" ? ACCESS_JWT_SECRET : REFRESH_JWT_SECRET;
+  const jwtSecret: Secret = tokenType === "access" ? ACCESS_JWT_SECRET : REFRESH_JWT_SECRET;
 
   const expiresIn: ms.StringValue = (
-    tokenType === "access"
-      ? process.env["ACCESS_JWT_EXPIRES_IN"]
-      : process.env["REFRESH_JWT_EXPIRES_IN"]
+    tokenType === "access" ? ACCESS_JWT_EXPIRES_IN : REFRESH_JWT_EXPIRES_IN
   ) as ms.StringValue;
 
   return jwt.sign(payload, jwtSecret, {
@@ -28,48 +25,31 @@ export function signAuthJWT(payload: object, tokenType: "access" | "refresh") {
 export async function verifyAuthJWT(
   token: string,
   type: "access" | "refresh",
-): Promise<JwtPayload> {
+): Promise<AuthJWTPayload> {
   const SECRET = type === "access" ? ACCESS_JWT_SECRET : REFRESH_JWT_SECRET;
 
   // Promisifying JWT verification to free up Node.js event loop:
-  const verifyAsync = promisify(
-    jwt.verify as (token: string, secret: string) => JwtPayload,
-  );
-  const decoded = (await verifyAsync(token, SECRET)) as JwtPayload;
+  const verifyAsync = promisify(jwt.verify as (token: string, secret: string) => JwtPayload);
+  const decoded = (await verifyAsync(token, SECRET)) as AuthJWTPayload;
 
   return decoded;
 }
 
-export function createRefreshToken(
-  user: HydratedDocument<IEmployee | IOrganization>,
-  payload: AuthJWTPayload,
-) {
-  const accountType = "firstName" in user ? "employee" : "organization";
-
+export function createRefreshToken(payload: AuthJWTPayload) {
   const refreshToken = signAuthJWT(payload, "refresh");
-  const expiresAt: Date = new Date(
-    Date.now() +
-      Number.parseInt(process.env["REFRESH_JWT_EXPIRES_IN"]!) *
-        24 *
-        60 *
-        60 *
-        1000,
+  const refreshTokenExpiry: Date = new Date(
+    Date.now() + Number.parseInt(REFRESH_JWT_EXPIRES_IN) * 24 * 60 * 60 * 1000,
   );
 
-  const tokenHash = crypto
-    .createHash("sha256")
-    .update(refreshToken)
-    .digest("hex");
+  const tokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
 
-  return { refreshToken, tokenHash, expiresAt };
+  return { refreshToken, tokenHash, refreshTokenExpiry };
 }
 
-export function createAccessToken(
-  user: HydratedDocument<IEmployee | IOrganization>,
-) {
-  const accountType = "firstName" in user ? "employee" : "organization";
-  const payload = {
-    id: user.id,
-    accountType,
-  };
+export function createAccessToken(payload: AuthJWTPayload) {
+  const accessToken = signAuthJWT(payload, "access");
+  const accessTokenExpiry: Date = new Date(
+    Date.now() + Number.parseInt(ACCESS_JWT_EXPIRES_IN) * 60 * 1000,
+  );
+  return { accessToken, accessTokenExpiry };
 }
