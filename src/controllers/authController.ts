ea3b7ce50e5b842cff1170299bd5b8c266ac3f4e
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-14 20:00:27 +0530

    Added/updated/corrected/refactored logic of auth handler functions

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,3 @@
-import crypto from "crypto";
 import mongoose from "mongoose";
 
 import { isAfter } from "date-fns";
@@ -7,21 +6,20 @@ import { DeviceSession } from "../models/deviceSessionModel.js";
 import { Employee, type EmployeeDocument } from "../models/employeeModel.js";
 import { Organization, type OrganizationDocument } from "../models/organizationModel.js";
 import type { AuthJWTPayload } from "../types/types.js";
+import { authenticateUser } from "../utils/authenticateUser.js";
 import { catchAsyncError } from "../utils/catchAsyncError.js";
-import Email from "../utils/email.js";
-import { generateToken } from "../utils/generateToken.js";
-import { createAccessToken, createRefreshToken, verifyAuthJWT } from "../utils/jwt.js";
+import { verifyAuthJWT } from "../utils/jwt.js";
 import {
   sendAccessTokenExpiredResponse,
+  sendInvalidCredentialsResponse,
   sendInvalidTokenResponse,
   sendNoAccessTokenResponse,
-  sendRefreshTokenExpiredResponse,
   sendReloginAfterPasswordChangeResponse,
-  sendSessionCompromisedResponse,
   sendUnverifiedEmailResponse,
   sendUserNotFoundResponse,
   sendVerificationWindowExpiredResponse,
-} from "../utils/unauthorizedResponses.js";
+} from "../utils/userAuthorizationResponses.js";
+import { validateRefreshToken } from "../utils/validateRefreshToken.js";
 
 export const signup = catchAsyncError(async (req, res, next) => {
   const accountType =
@@ -32,84 +30,45 @@ export const signup = catchAsyncError(async (req, res, next) => {
     | EmployeeDocument
     | OrganizationDocument;
 
-  const { token: verificationToken, hashedToken: hashedVerificationToken } = generateToken();
-  newUser.emailVerificationToken = hashedVerificationToken;
-  newUser.emailVerificationExpires = new Date(Date.now() + 24 * 60 * 60 * 1000);
-  await newUser.save();
-
   const jwtPayload: AuthJWTPayload = { id: newUser.id, accountType };
-  const { refreshToken, tokenHash, refreshTokenExpiry } = createRefreshToken(jwtPayload);
-  const { accessToken, accessTokenExpiry } = createAccessToken(jwtPayload);
-  const userAgent = req.headers["user-agent"];
-  const ipAddress = req.ip;
-
-  const emailVerificationUrl = `${req.protocol}://${req.get("host")}/verify-email/${verificationToken}`;
-  await new Email(newUser, emailVerificationUrl).sendWelcome();
-
-  await DeviceSession.create({
-    userId: new mongoose.Types.ObjectId(newUser.id),
-    userType: accountType,
-    tokenHash,
-    ...(userAgent ? { userAgent } : {}),
-    ...(ipAddress ? { ipAddress } : {}),
-  });
-
-  res.cookie("refresh_token", refreshToken, {
-    expires: refreshTokenExpiry,
-    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-    httpOnly: true,
-    sameSite: "strict",
-  });
-
-  res.status(201).json({
-    message: "success",
-    accessToken,
-    tokenExpiresAt: accessTokenExpiry,
-  });
+  return authenticateUser({ req, res, jwtPayload, user: newUser, authAction: "signup" });
 });
 
 export const login = catchAsyncError(async function (req, res, next) {
   const refreshToken = req.cookies["refresh_token"];
 
   if (refreshToken) {
-    const tokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
-    const { id, accountType, ...decoded } = await verifyAuthJWT(refreshToken, "refresh");
-    const UserModel = mongoose.model(accountType);
-    const userId = new mongoose.Types.ObjectId(id);
-    const session = await DeviceSession.findOne({ userId, tokenHash });
-    const user = (await UserModel.findById(userId).setOptions({
-      includeUnverified: true,
-    })) as EmployeeDocument | OrganizationDocument;
-    if (!session || !user) {
-      // Possible token reuse attack, so log the user out of all devices:
-      await DeviceSession.deleteMany({ userId });
-      return !session ? sendSessionCompromisedResponse(res) : sendUserNotFoundResponse(res);
+    const refreshTokenIsValid = await validateRefreshToken(req, res, refreshToken);
+    if (!refreshTokenIsValid) return;
+
+    if (refreshTokenIsValid) {
+      const { id, accountType } = await verifyAuthJWT(refreshToken, "refresh");
+      const UserModel = mongoose.model(accountType);
+      const userId = new mongoose.Types.ObjectId(id);
+      const user = (await UserModel.findOne({ _id: userId }).setOptions({
+        includeUnverified: true,
+      })) as EmployeeDocument | OrganizationDocument;
+      const jwtPayload: AuthJWTPayload = { id, accountType };
+      return authenticateUser({ req, res, jwtPayload, user, authAction: "login" });
     }
-
-    if (isAfter(new Date(), decoded.exp!)) {
-      await DeviceSession.deleteOne({ userId, tokenHash });
-      return sendRefreshTokenExpiredResponse(res);
-    }
-
-    await session.deleteOne();
-
-    const payload: AuthJWTPayload = { id, accountType };
-    const {
-      refreshToken: newRefreshToken,
-      tokenHash: newTokenHash,
-      refreshTokenExpiry,
-    } = createRefreshToken(payload);
-    const { accessToken, accessTokenExpiry } = createAccessToken(payload);
   }
 
   const { email, password } = req.body;
   if (!email || !password) return next(new AppError("Invalid credentials", 400));
 
   const user = (
-    await Promise.allSettled([Employee.findOne({ email }), Organization.findOne({ email })])
+    await Promise.allSettled([
+      Employee.findOne({ email }).select("+password"),
+      Organization.findOne({ email }).select("+password"),
+    ])
   ).filter((result) => result.status === "fulfilled")?.[0]?.value;
 
-  if (!user) sendUserNotFoundResponse(res);
+  const passwordsMatched = await user?.matchPasswords(password, user?.password || "");
+  if (!user || !passwordsMatched) return sendInvalidCredentialsResponse(res);
+
+  const accountType = "firstName" in user ? "Employee" : "Organization";
+  const jwtPayload: AuthJWTPayload = { id: user.id, accountType };
+  authenticateUser({ req, res, jwtPayload, user, authAction: "login" });
 });
 
 export const protect = catchAsyncError(async function (req, res, next) {
@@ -121,7 +80,7 @@ export const protect = catchAsyncError(async function (req, res, next) {
 
   const decoded = await verifyAuthJWT(accessToken, "access");
   if (!decoded || !decoded.iat || !decoded.exp) return sendInvalidTokenResponse(res);
-  if (decoded.exp < Date.now()) return sendAccessTokenExpiredResponse(res);
+  if (decoded.exp * 1000 < Date.now()) return sendAccessTokenExpiredResponse(res);
 
   const userId = new mongoose.Types.ObjectId(decoded.id);
   const UserModel = mongoose.model(decoded.accountType);
@@ -136,8 +95,8 @@ export const protect = catchAsyncError(async function (req, res, next) {
 
   if (!sessionUser.emailIsVerified) return sendUnverifiedEmailResponse(res);
 
-  if ((sessionUser as EmployeeDocument).passwordChangedAfter(decoded.iat))
+  if ((sessionUser as EmployeeDocument).changedPasswordAfter(decoded.iat))
     return sendReloginAfterPasswordChangeResponse(res);
 
-  next();
+  return next();
 });
