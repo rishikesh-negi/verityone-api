a8e42f2f8e646a23c8fb00b2f8ae6a663303b4ce
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-15 19:49:57 +0530

    Renamed the module and exported function to a more logical name

--- src/utils/validateSession.ts
+++ src/utils/validateSession.ts
@@ -1,36 +1,20 @@
 import crypto from "crypto";
-import type { Request, Response } from "express";
 import mongoose from "mongoose";
 import { DeviceSession } from "../models/deviceSessionModel.js";
 import type { EmployeeDocument } from "../models/employeeModel.js";
 import type { OrganizationDocument } from "../models/organizationModel.js";
 import { verifyAuthJWT } from "./jwt.js";
-import {
-  triggerRefreshJWTCookieRemoval,
-  sendInvalidTokenResponse,
-  sendReloginAfterPasswordChangeResponse,
-  sendSessionCompromisedResponse,
-  sendSessionExpiredResponse,
-  sendUserNotFoundResponse,
-} from "./userAuthorizationResponses.js";
 
-export async function validateRefreshToken(
-  req: Request,
-  res: Response,
-  refreshToken: string,
-): Promise<boolean> {
+export async function validateSession(refreshToken: string): Promise<boolean> {
   const { id, accountType, ...decoded } = await verifyAuthJWT(refreshToken, "refresh");
-  if (!id || !accountType || !decoded.iat || !decoded.exp) {
+  if (
+    !id ||
+    !accountType ||
+    !decoded.iat ||
+    !decoded.exp ||
+    (accountType !== "Employee" && accountType !== "Organization")
+  ) {
     await DeviceSession.deleteMany({ userId: new mongoose.Types.ObjectId(id) });
-    triggerRefreshJWTCookieRemoval(req, res);
-    sendInvalidTokenResponse(res);
-    return false;
-  }
-
-  if (accountType !== "Employee" && accountType !== "Organization") {
-    await DeviceSession.deleteMany({ userId: new mongoose.Types.ObjectId(id) });
-    triggerRefreshJWTCookieRemoval(req, res);
-    sendInvalidTokenResponse(res);
     return false;
   }
 
@@ -43,25 +27,19 @@ export async function validateRefreshToken(
   })) as EmployeeDocument | OrganizationDocument;
 
   if (!session || !user) {
-    // Possible token theft or reuse attack, so log the user out of all devices:
+    // Possible token theft or reuse attack, so log the user out of all devices and try clearing the refresh token cookie on the client:
     await DeviceSession.deleteMany({ userId });
-    triggerRefreshJWTCookieRemoval(req, res);
-    if (!session) sendSessionCompromisedResponse(res);
-    if (!user) sendUserNotFoundResponse(res);
     return false;
   }
 
   if (Date.now() > decoded.exp * 1000) {
     await session.deleteOne();
-    triggerRefreshJWTCookieRemoval(req, res);
-    sendSessionExpiredResponse(res);
     return false;
   }
 
   // Type-casting "user" to EmployeeDocument to get rid of the TS compile-time error raised due to the unavoidable but safe mismatch between the context types of "user" and the .changedPasswordAfter() method:
   if ((user as EmployeeDocument).changedPasswordAfter(decoded.iat)) {
-    triggerRefreshJWTCookieRemoval(req, res);
-    sendReloginAfterPasswordChangeResponse(res);
+    await DeviceSession.deleteMany({ userId });
     return false;
   }
 
