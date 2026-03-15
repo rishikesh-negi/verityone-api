1993ab74c4aac84e03068de8b9131eecfd35a373
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-15 19:54:15 +0530

    Added/updated/corrected error handling logic

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -3,23 +3,21 @@ import { AppError } from "../errors/AppError.js";
 import { transformError } from "../errors/transformError.js";
 import { GENERIC_ERROR_MSG } from "../utils/constants.js";
 
-export type SendErrorFunction = (
-  err: Error | AppError,
-  req: Request,
-  res: Response,
-) => void;
+export type SendErrorFunction = (err: Error | AppError, req: Request, res: Response) => void;
 
-function generateErrorResponse(
+function sendErrorResponse(
   err: Error | AppError,
   res: Response,
   message: string = GENERIC_ERROR_MSG,
 ) {
   const status = err instanceof AppError ? err.status : "error";
   const statusCode = err instanceof AppError ? err.statusCode : 500;
+  const reason = err instanceof AppError ? err.reason : null;
 
   return res.status(statusCode).json({
     status,
-    message: message,
+    ...(reason ? { reason } : {}),
+    message,
   });
 }
 
@@ -29,37 +27,34 @@ const sendErrorInDevelopment: SendErrorFunction = function (err, req, res) {
   if (req.originalUrl.startsWith("/api")) {
     const statusCode = err instanceof AppError ? err.statusCode : 500;
     const status = err instanceof AppError ? err.status : "error";
+    const reason = err instanceof AppError ? err.reason : null;
 
     return res.status(statusCode).json({
       status,
-      error: err,
+      ...(reason ? { reason } : {}),
       message: err.message,
+      error: err,
       stack: err.stack,
     });
   }
 
   // Sending an error response for non-API errors:
-  return generateErrorResponse(err, res, err.message);
+  return sendErrorResponse(err, res, err.message);
 };
 
 // -----> Function to send an error response in production:
 const sendErrorInProduction: SendErrorFunction = function (err, req, res) {
-  // Sending an error response for invalid/failed API requests:
+  // Error response for invalid/failed API requests:
   if (req.originalUrl.startsWith("/api")) {
-    // Operational error - Send message to the client:
-    if (err instanceof AppError)
-      return generateErrorResponse(err, res, err.message);
-
-    // Programming or unknown error - Log it to the console and send a generic error message to the client:
-    console.error("ERROR: ", err);
-    return generateErrorResponse(err, res, GENERIC_ERROR_MSG);
+    // If operational error, send actual error message. If programming or unknown error, send a generic error message:
+    const errorMessage = err instanceof AppError ? err.message : GENERIC_ERROR_MSG;
+    return sendErrorResponse(err, res, errorMessage);
   }
 
   // Non-API error:
-  if (err instanceof AppError)
-    return generateErrorResponse(err, res, err.message);
+  if (err instanceof AppError) return sendErrorResponse(err, res, err.message);
 
-  return generateErrorResponse(err, res, GENERIC_ERROR_MSG);
+  return sendErrorResponse(err, res, GENERIC_ERROR_MSG);
 };
 
 export function globalErrorHandler(
@@ -68,15 +63,13 @@ export function globalErrorHandler(
   res: Response,
   _next: NextFunction,
 ): void {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
   if (process.env["NODE_ENV"] === "development") {
     sendErrorInDevelopment(err, req, res);
   }
 
   if (process.env["NODE_ENV"] === "production") {
-    const error = transformError(err);
+    // An operational error containing a 'reason' is intended to be sent as it is:
+    const error = err.reason ? err : transformError(err);
     sendErrorInProduction(error, req, res);
   }
 }
