53ea0f0878c4a3883b41aef7f7b9168b747e6412
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-15 19:56:55 +0530

    Created a reusable app constant for refresh token cookie name

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,5 @@ export const EMPLOYEE_NAME_MAX_LENGTH = 40;
 
 export const DEVICE_SESSION_VALIDITY_IN_SECONDS =
   Number.parseInt(process.env["REFRESH_JWT_EXPIRES_IN"]!) * 24 * 60 * 60;
+
+export const REFRESH_JWT_COOKIE_NAME = "refresh_token";
