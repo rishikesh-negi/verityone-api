a3d87954980fc9ed2c4500faef2f9beb37895889
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-29 19:48:27 +0530

    Created a Mongoose schema and model for survey responses

--- src/models/surveyResponseModel.ts
+++ src/models/surveyResponseModel.ts
@@ -1,52 +1,35 @@
-import { Schema } from "mongoose";
+import { model, Schema, type InferSchemaType } from "mongoose";
+import { metrics, numQuestions, questionLabels } from "../data/surveyQuestions.js";
 
-const surveyQuestionsSchema = new Schema({
-  socialHarmony: {
-    type: {
-      unfairDiscrimination: {
-        type: {
-          label: {
-            type: String,
-            default: "unfair-discrimination",
-            immutable: true,
-          },
-          question: {
-            type: String,
-            default:
-              "How often do employees experience unfair discrimination from colleagues or superiors at <ORGANIZATION>?",
-            immutable: true,
-          },
+type SurveyQuestionResponse = {
+  metric: number;
+  label: string;
+  response: number;
+};
+
+const surveyResponseSchema = new Schema({
+  responses: {
+    required: true,
+    type: [
+      {
+        metric: { required: true, type: String, enum: metrics },
+        label: { required: true, type: String, enum: questionLabels },
+        response: {
+          required: true,
+          type: Number,
+          min: [1, "Response score should be at least 1"],
+          max: [10, "Response score cannot exceed 10"],
         },
       },
-      workplaceIsolation: {
-        type: {
-          label: {
-            type: String,
-            default: "workplace-isolation",
-            immutable: true,
-          },
-          question: {
-            type: String,
-            default:
-              "How often do you feel isolated from colleagues or superiors at <ORGANIZATION>?",
-            immutable: true,
-          },
-        },
-      },
-      officePoliticsPrevalence: {
-        type: {
-          label: {
-            type: String,
-            default: "office-politics-prevalence",
-            immutable: true,
-          },
-          question: {
-            type: String,
-            default: "How common or prevalent is office politics at <ORGANIZATION>?",
-            immutable: true,
-          },
-        },
+    ],
+    validate: {
+      validator: function (v: SurveyQuestionResponse[]) {
+        return v.length === numQuestions;
       },
     },
   },
 });
+
+export type ISurveyResponseSchema = InferSchemaType<typeof surveyResponseSchema>;
+
+export const SurveyResponse = model<ISurveyResponseSchema>("SurveyResponse", surveyResponseSchema);
