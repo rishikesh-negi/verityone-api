92a6a74fc0dd4ac92a4d13cb07443a48f770eeca
Rishikesh Negi <rishinegi97@gmail.com>
2026-03-31 19:15:37 +0530

    Added immutability to unchanging fields and updated error messages

--- src/models/userIdentityVaultModel.ts
+++ src/models/userIdentityVaultModel.ts
@@ -3,16 +3,19 @@ import { model, Schema, type InferSchemaType } from "mongoose";
 const userIdentityVaultSchema = new Schema({
   userId: {
     type: Schema.ObjectId,
+    immutable: true,
     ref: "Employee",
     required: [true, "User ID is required for creating an anonymous identity"],
   },
   anonymousId: {
     type: Schema.Types.UUID,
-    requried: [true, "A UUID is required for mapping reviews to the anonymous identity"],
+    immutable: true,
+    requried: [true, "A UUID is required to create the user identity vault"],
   },
   tokenHash: {
     type: String,
-    required: [true, "A token is required for tokenized access to the anonymous identity"],
+    immutable: true,
+    required: [true, "A token is required to authorize review creation"],
   },
   active: { type: Boolean, default: true, select: false },
   createdAt: { type: Date, default: Date.now(), select: false },
