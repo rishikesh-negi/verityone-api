70797a2fc294f7b38bf8590d85e65b5929c93aa0
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-01 20:17:42 +0530

    Corrected the computation of the module's absolute path

--- src/utils/renderEmailTemplate.ts
+++ src/utils/renderEmailTemplate.ts
@@ -1,12 +1,13 @@
 import { readFile } from "fs/promises";
-
+import path from "path";
 import handlebars from "handlebars";
+import { fileURLToPath } from "url";
 
-export async function renderEmailTemplate(
-  template: string,
-  data?: Record<string, unknown>,
-) {
-  const templatePath = `${__dirname}/../views/emails/${template}.hbs`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export async function renderEmailTemplate(template: string, data?: Record<string, unknown>) {
+  const templatePath = `${__dirname}/../views/emails/templates/${template}.hbs`;
   const templateFile = await readFile(templatePath, "utf-8");
   const compiledTemplate = handlebars.compile(templateFile);
   return compiledTemplate(data);
