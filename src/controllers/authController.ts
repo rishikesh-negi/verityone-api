6612bb92f3c7b6e826d26f9f1fa728559454e74f
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-01 20:22:15 +0530

    Added: Missing logic to clear refresh token and delete sessions after password change in the protect middleware, and added logic for handling user email verification in the verification middleware

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -129,16 +129,43 @@ export const protect = catchAsyncError(async function (req, res, next) {
 
   if (!sessionUser.emailIsVerified) return next(new UnverifiedEmailError());
 
-  if ((sessionUser as EmployeeDocument).changedPasswordAfter(decoded.iat))
+  if ((sessionUser as EmployeeDocument).changedPasswordAfter(decoded.iat)) {
+    triggerRefreshJWTCookieRemoval(req, res);
+    await DeviceSession.deleteMany({ userId });
     return next(new PasswordChangedReloginError());
+  }
 
   req.user = sessionUser;
   return next();
 });
 
 export const verifyEmailAddress = catchAsyncError(async function (req, res, next) {
-  const { verificationToken } = req.params;
-  if (!verificationToken) return next(new InvalidEmailVerificationToken());
+  const rawToken = req.params["verificationToken"] as string;
+  if (!rawToken) return next(new InvalidEmailVerificationToken());
+
+  const emailVerificationToken = crypto.createHash("sha256").update(rawToken).digest("hex");
+
+  const user = (
+    await Promise.allSettled([
+      Employee.findOne({ emailVerificationToken }),
+      Organization.findOne({ emailVerificationToken }),
+    ])
+  ).filter((result) => result.status === "fulfilled")?.[0]?.value as
+    | EmployeeDocument
+    | OrganizationDocument;
+
+  if (!user) return next(new InvalidEmailVerificationToken());
+
+  if (Date.now() > user.emailVerificationExpires!.getTime()) {
+    await mongoose.model(user.userType).deleteOne({ _id: user.id });
+    await DeviceSession.deleteMany({ userId: user.id });
+    return next(new VerificationWindowExpiredError());
+  }
+
+  user.emailIsVerified = true;
+  user.emailVerificationExpires = null;
+  user.emailVerificationToken = null;
+  await user.save();
 });
 
 export const restrictToVerified = catchAsyncError(async function (req, _res, next) {
