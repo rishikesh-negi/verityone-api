56f83777142e5278c4763fcc93072f14c774a331
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-29 19:35:35 +0530

    Create Mongoose model for employee onboarding invites

---
+++ src/models/onboardingInviteModel.ts
@@ -0,0 +1,23 @@
+import { model, Schema, type InferSchemaType } from "mongoose";
+
+const onboardingInviteSchema = new Schema(
+  {
+    organization: { type: Schema.Types.ObjectId, ref: "Organization", required: true },
+    employee: { type: Schema.Types.ObjectId, ref: "Employee", required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      expires: 6 * 30.5 * 24 * 60 * 60,
+      select: false,
+      immutable: true,
+    },
+  },
+  { timestamps: true },
+);
+
+type IOnboardingInvite = InferSchemaType<typeof onboardingInviteSchema>;
+
+export const OnboardingInvite = model<IOnboardingInvite>(
+  "OnboardingInvite",
+  onboardingInviteSchema,
+);
