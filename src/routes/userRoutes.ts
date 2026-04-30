26bb79121fed132e08c12177b8b7bd344e966ed4
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-30 19:45:48 +0530

    Add the middleware to protect the upcoming protected routes from unauthenticated/unauthorized/unverified users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router, type RequestHandler } from "express";
-import { login, logout, signup } from "../controllers/authController.js";
+import { login, logout, protect, signup } from "../controllers/authController.js";
 import { validateRequest } from "../middleware/validateRequest.js";
 import { loginCredentialsSchema, signupRequestSchema } from "../validations/auth.validation.js";
 
@@ -8,3 +8,7 @@ const router = Router();
 router.post("/signup", validateRequest(signupRequestSchema) as RequestHandler, signup);
 router.post("/login", validateRequest(loginCredentialsSchema) as RequestHandler, login);
 router.get("/logout", logout);
+
+router.use(protect);
+
+export default router;
