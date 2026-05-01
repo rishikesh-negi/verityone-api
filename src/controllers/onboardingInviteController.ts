8a18d7bfeabe4b41770c1cc530c64c5562574a2b
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-01 19:52:22 +0530

    Update route handler to access URL params instead of request body and add an important missing validation

--- src/controllers/onboardingInviteController.ts
+++ src/controllers/onboardingInviteController.ts
@@ -1,23 +1,21 @@
 import type { NextFunction, Response } from "express";
-import { UnprocessableContentError } from "../errors/AppError.js";
-import type { RequestWithUser } from "../types/types.js";
-import { catchAsyncError } from "../utils/catchAsyncError.js";
-import { Organization } from "../models/organizationModel.js";
+import { AppError, UnprocessableContentError } from "../errors/AppError.js";
 import { Employee } from "../models/employeeModel.js";
 import { OnboardingInvite } from "../models/onboardingInviteModel.js";
+import type { RequestWithUser } from "../types/types.js";
+import { catchAsyncError } from "../utils/catchAsyncError.js";
 
 export const createInvite = catchAsyncError(
   async (req: RequestWithUser, res: Response, next: NextFunction) => {
-    const { organization: orgId, employee: employeeId } = req.body;
-    if (!orgId || !employeeId) return next(new UnprocessableContentError());
+    const { employeeId } = req.params;
+    if (!employeeId) return next(new UnprocessableContentError());
 
-    const [organizationExists, employeeExists] = await Promise.all([
-      Organization.exists({ _id: orgId }),
-      Employee.exists({ _id: employeeId }),
-    ]);
-    if (!organizationExists || !employeeExists) return next(new UnprocessableContentError());
+    const employee = await Employee.findById(employeeId);
+    if (!employee) return next(new UnprocessableContentError());
+    if (employee.organization !== null)
+      return next(new AppError("Employee is part of another organization", 403));
 
-    await OnboardingInvite.create({ organization: orgId, employee: employeeId });
+    await OnboardingInvite.create({ organization: req.user!.id, employee: employeeId });
     return res.status(201).json({
       status: "success",
       message: "Onboarding invite sent to the employee",
