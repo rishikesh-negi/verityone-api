15b34c81da8a0aaa5ca0ac97ff6f7d6e4ba8d0ce
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-02 18:38:27 +0530

    Add Zod schema to validate invite acceptance/rejection request structure

--- src/validations/onboardingInvite.validation.ts
+++ src/validations/onboardingInvite.validation.ts
@@ -1,8 +1,10 @@
 import z from "zod";
 import { ObjectId } from "mongodb";
 
-export const inviteSchema = z.object({
-  params: z.object({
-    employeeId: z.string().refine((val) => ObjectId.isValid(val), { error: "Invalid user ID" }),
-  }),
-});
+export const documentIdSchema = z
+  .string()
+  .refine((val) => ObjectId.isValid(val), { error: "Invalid resource ID" });
+
+export const inviteSchema = z.object({ params: z.object({ employeeId: documentIdSchema }) });
+
+export const inviteActionSchema = z.object({ params: z.object({ inviteId: documentIdSchema }) });
