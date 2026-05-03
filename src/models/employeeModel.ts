94f9835bd205df40d16a190c5300386ae3d7d39d
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-03 19:09:08 +0530

    Refactor Organization document field names into a reusable string for use in the populate method's 'select' option

--- src/models/employeeModel.ts
+++ src/models/employeeModel.ts
@@ -14,6 +14,7 @@ import {
   passwordValidator,
   usernameValidator,
 } from "../utils/stringValidators.js";
+import { organizationRefPopulateFields } from "../utils/constants.js";
 
 const employeeSchema = new Schema(
   {
@@ -114,7 +115,7 @@ employeeSchema.pre(/^find/, async function (this: Query<unknown, IEmployee>) {
 employeeSchema.pre(/^find/, async function (this: Query<unknown, IEmployee>) {
   this.populate({
     path: "organization",
-    select: "name slug postalCode city country ratingsAverage numRatings",
+    select: organizationRefPopulateFields,
   });
 });
 
