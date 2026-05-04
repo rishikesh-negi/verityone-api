0066435ab55e18723222114f38a6ebf2540d4457
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-04 17:19:16 +0530

    Create types for invite creation and invite action input data from Zod schemas

--- src/validations/onboardingInvite.validation.ts
+++ src/validations/onboardingInvite.validation.ts
@@ -6,5 +6,7 @@ export const documentIdSchema = z
   .refine((val) => ObjectId.isValid(val), { error: "Invalid resource ID" });
 
 export const inviteSchema = z.object({ params: z.object({ employeeId: documentIdSchema }) });
+export type InviteCreationInputData = z.infer<typeof inviteSchema>;
 
 export const inviteActionSchema = z.object({ params: z.object({ inviteId: documentIdSchema }) });
+export type InviteActionInputData = z.infer<typeof inviteActionSchema>;
