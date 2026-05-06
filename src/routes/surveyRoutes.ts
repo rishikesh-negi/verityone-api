261ec3e1dafe024d5485e1d8f62137ec2d4ebb11
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-06 19:18:41 +0530

    Created dedicated Express router for survey endpoints

---
+++ src/routes/surveyRoutes.ts
@@ -0,0 +1,16 @@
+import { Router, type RequestHandler } from "express";
+import { protect, restrictTo } from "../controllers/authController.js";
+import { createSurvey } from "../controllers/surveyController.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { surveyCreationSchema } from "../validations/survey.validation.js";
+
+const router = Router();
+
+router.use(protect);
+
+router.post(
+  "/create",
+  restrictTo("Organization"),
+  validateRequest(surveyCreationSchema) as RequestHandler,
+  createSurvey,
+);
