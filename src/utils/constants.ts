e2c07391d9543b6f11b25d0c83092f5504c8b508
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-09 19:03:35 +0530

    Update name of renamed schema field

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,7 +11,7 @@ export const REFRESH_JWT_COOKIE_NAME = "refresh_token";
 export const INVITE_VALIDITY_SECONDS = 3 * 30.5 * 24 * 60 * 60;
 
 export const ORG_FIELDS_TO_POPULATE =
-  "_id name slug username email postalCode city country ratingsAverage numRatings";
+  "_id name slug username email postalCode city country overallRating numRatings";
 
 export const SURVEY_DURATION_DAYS_OPTIONS: number[] = [7, 30, 90];
 
