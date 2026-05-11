36c4f9d15e75bc71da3334ddb0983819e9027106
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-11 18:36:03 +0530

    Begin writing route handlers for SSE updates

---
+++ src/controllers/sseController.ts
@@ -0,0 +1,26 @@
+import type { Response } from "express";
+import type { EmployeeDocument } from "../models/employeeModel.js";
+import { sseService } from "../services/sseService.js";
+import type { RequestWithUser } from "../types/types.js";
+
+export const startSSE = (req: RequestWithUser, res: Response) => {
+  const { organization } = req.user as EmployeeDocument;
+  if (!organization)
+    return res
+      .status(403)
+      .json({ status: "fail", error: "Only onboarded employees may receive survey updates" });
+
+  const orgId = organization._id.toString();
+
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  sseService.addClient(orgId, res);
+
+  const heartbeat = setInterval(() => {
+    res.write(": keep-alive\n\n");
+  }, 30_000);
+
+  res.on("close", () => clearInterval(heartbeat));
+};
