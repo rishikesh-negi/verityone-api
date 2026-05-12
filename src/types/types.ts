d352de7f0135f279a595e0551dadc34f65449a4f
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-12 19:17:37 +0530

    Create a type for the response objects of SSE connections

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,6 +8,10 @@ export interface RequestWithUser extends Request {
   user?: EmployeeDocument | OrganizationDocument;
 }
 
+export interface SSEResponseStream extends Response {
+  subscriberId: string;
+}
+
 export type SyncRouteHandler = (req: RequestWithUser, res: Response, next: NextFunction) => void;
 
 export type AsyncRouteHandler = (
