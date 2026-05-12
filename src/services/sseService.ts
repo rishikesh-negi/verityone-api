0bcdf99039fba0cdace2ba70a0fd7d4673282432
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-12 19:16:41 +0530

    Refactor singleton structure to connect to all authenticated clients to stream common, public, and generic updates

--- src/services/sseService.ts
+++ src/services/sseService.ts
@@ -7,23 +7,23 @@ class SSEService {
     this.clients = new Map();
   }
 
-  addClient(orgId: string, res: Response) {
-    if (!this.clients.has(orgId)) this.clients.set(orgId, new Set());
-    this.clients.get(orgId)?.add(res);
+  addClient(res: Response, classifierKey: string = "all") {
+    if (!this.clients.has(classifierKey)) this.clients.set(classifierKey, new Set());
+    this.clients.get(classifierKey)?.add(res);
 
     res.on("close", () => {
-      this.clients.get(orgId)?.delete(res);
-      if (this.clients.get(orgId)?.size === 0) this.clients.delete(orgId);
+      this.clients.get(classifierKey)?.delete(res);
+      if (this.clients.get(classifierKey)?.size === 0) this.clients.delete(classifierKey);
     });
   }
 
-  sendToEmployees(orgId: string, data: Record<string, unknown>, event?: string) {
-    const orgEmployees = this.clients.get(orgId);
-    if (orgEmployees) {
+  sendToClients(data: Record<string, unknown>, classifierKey: string = "all", event?: string) {
+    const clients = this.clients.get(classifierKey);
+    if (clients) {
       const payload = `data: ${JSON.stringify(data)}\n\n`;
-      orgEmployees.forEach((emp) => {
-        if (event) emp.write(`event: ${event}\n\n`);
-        emp.write(payload);
+      clients.forEach((clientRes) => {
+        if (event) clientRes.write(`event: ${event}\n\n`);
+        clientRes.write(payload);
       });
     }
   }
