b6c562f01027a4027f5f74b455d197b764f9959b
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-12 19:14:27 +0530

    Add route handler to create SSE connection for general updates on client request

--- src/controllers/sseController.ts
+++ src/controllers/sseController.ts
@@ -1,7 +1,37 @@
-import type { Response } from "express";
+import type { CookieOptions, Request, Response } from "express";
 import type { EmployeeDocument } from "../models/employeeModel.js";
 import { sseService } from "../services/sseService.js";
-import type { RequestWithUser } from "../types/types.js";
+import type { RequestWithUser, SSEResponseStream } from "../types/types.js";
+import { v7 as uuidv7 } from "uuid";
+
+export const subscribeToPublicSSE = (req: Request, res: SSEResponseStream) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  res.flushHeaders();
+
+  res.subscriberId = uuidv7();
+
+  sseService.addClient(res);
+  const sseSubscriberIdCookieOptions: CookieOptions = {
+    httpOnly: true,
+    secure:
+      process.env["NODE_ENV"] === "development"
+        ? false
+        : req.secure || req.headers["x-forwarded-proto"] === "https",
+    sameSite: process.env["NODE_ENV"] === "development" ? "lax" : "strict",
+  };
+
+  res.cookie("sse_subscriber_id", res.subscriberId, sseSubscriberIdCookieOptions);
+
+  const heartbeat = setInterval(() => res.write(": keep-alive"), 30_000);
+
+  res.on("close", () => {
+    clearInterval(heartbeat);
+    res.clearCookie("sse_subscriber_id", sseSubscriberIdCookieOptions);
+  });
+};
 
 export const startSSE = (req: RequestWithUser, res: Response) => {
   const { organization } = req.user as EmployeeDocument;
@@ -18,11 +48,5 @@ export const startSSE = (req: RequestWithUser, res: Response) => {
 
   res.flushHeaders();
 
-  sseService.addClient(orgId, res);
-
-  const heartbeat = setInterval(() => {
-    res.write(": keep-alive\n\n");
-  }, 30_000);
-
-  res.on("close", () => clearInterval(heartbeat));
+  sseService.addClient(res, orgId);
 };
