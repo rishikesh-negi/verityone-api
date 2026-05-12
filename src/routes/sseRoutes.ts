1a04e60fabde03987e8f2d18fa8f071ea464baa0
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-12 19:15:21 +0530

    Add route to allow clients to create an SSE connection

--- src/routes/sseRoutes.ts
+++ src/routes/sseRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { protect, restrictTo } from "../controllers/authController.js";
+import { subscribeToPublicSSE } from "../controllers/sseController.js";
 
 const router = Router();
 
 router.use(protect);
 
+router.get("/events", subscribeToPublicSSE);
 router.get("/events/new-survey", restrictTo("Employee"));
