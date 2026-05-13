36f0060b066db55d241af95b06fbfd3da65c48d1
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-13 18:07:43 +0530

    Refactor the SSE service singleton logic for the updated structure of the wrapper containing the persistent response object

--- src/services/sseService.ts
+++ src/services/sseService.ts
@@ -1,18 +1,18 @@
-import type { Response } from "express";
+import type { SSESubscriberClient } from "../types/types.js";
 
 class SSEService {
-  clients: Map<string, Set<Response>>;
+  clients: Map<string, Set<SSESubscriberClient>>;
 
   constructor() {
     this.clients = new Map();
   }
 
-  addClient(res: Response, classifierKey: string = "all") {
+  addClient(client: SSESubscriberClient, classifierKey: string = "all") {
     if (!this.clients.has(classifierKey)) this.clients.set(classifierKey, new Set());
-    this.clients.get(classifierKey)?.add(res);
+    this.clients.get(classifierKey)?.add(client);
 
-    res.on("close", () => {
-      this.clients.get(classifierKey)?.delete(res);
+    client.res.on("close", () => {
+      this.clients.get(classifierKey)?.delete(client);
       if (this.clients.get(classifierKey)?.size === 0) this.clients.delete(classifierKey);
     });
   }
@@ -21,9 +21,9 @@ class SSEService {
     const clients = this.clients.get(classifierKey);
     if (clients) {
       const payload = `data: ${JSON.stringify(data)}\n\n`;
-      clients.forEach((clientRes) => {
-        if (event) clientRes.write(`event: ${event}\n\n`);
-        clientRes.write(payload);
+      clients.forEach((client) => {
+        if (event) client.res.write(`event: ${event}\n\n`);
+        client.res.write(payload);
       });
     }
   }
