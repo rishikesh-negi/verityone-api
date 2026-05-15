1d903184bea952f38bbd0d938434246d5c5a6dfd
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-15 18:36:25 +0530

    Add the important missing fields to the schema and create a unique compound index to ensure single survey response from each participant

--- src/models/surveyResponseModel.ts
+++ src/models/surveyResponseModel.ts
@@ -1,39 +1,59 @@
 import { model, Schema, type InferSchemaType } from "mongoose";
-import { surveyMetrics, numQuestions, surveyCruxes } from "../data/surveyQuestions.js";
+import { numQuestions, surveyCruxes, surveyMetrics } from "../data/surveyQuestions.js";
 
-type SurveyQuestionResponse = {
+type SurveyQuestionAnswer = {
   metric: number;
   crux: string;
-  response: number;
+  answer: number;
 };
 
-const surveyResponseSchema = new Schema({
-  responses: {
-    required: true,
-    type: [
-      {
-        metric: { required: true, type: String, enum: surveyMetrics },
-        crux: { required: true, type: String, enum: surveyCruxes },
-        response: {
-          required: true,
-          type: Number,
-          min: [1, "Response score should be at least 1"],
-          max: [10, "Response score cannot exceed 10"],
-        },
-      },
-    ],
-    validate: {
-      validator: (v: SurveyQuestionResponse[]) => {
-        if (v.length !== numQuestions) return false;
-        const responseCruxesSet = new Set(v.map((answer) => answer.crux));
-        if (responseCruxesSet.size !== numQuestions) return false;
-        if (!surveyCruxes.every((crux) => responseCruxesSet.has(crux))) return false;
-        return true;
-      },
-      message: "Invalid or unprocessable survey response",
+const surveyResponseSchema = new Schema(
+  {
+    survey: {
+      type: Schema.Types.ObjectId,
+      ref: "Survey",
+      required: [true, "A response must belong to a survey"],
+      immutable: true,
+      index: true,
     },
+    anonymousId: {
+      type: Schema.Types.UUID,
+      ref: "UserIdentityVault",
+      required: [true, "This field is required to verify genuine employee participation"],
+      immutable: true,
+      index: true,
+    },
+    answers: {
+      required: [true, "Cannot create a survey response without answers"],
+      type: [
+        {
+          metric: { type: String, required: true, enum: surveyMetrics },
+          crux: { type: String, required: true, enum: surveyCruxes },
+          answer: {
+            type: Number,
+            required: true,
+            min: [1, "Answer cannot be less than 1"],
+            max: [10, "Answer cannot exceed 10"],
+          },
+        },
+      ],
+      validate: {
+        validator: (v: SurveyQuestionAnswer[]) => {
+          if (v.length !== numQuestions) return false;
+          const responseCruxesSet = new Set(v.map((answer) => answer.crux));
+          if (responseCruxesSet.size !== numQuestions) return false;
+          if (!surveyCruxes.every((crux) => responseCruxesSet.has(crux))) return false;
+          return true;
+        },
+        message: "Invalid or unprocessable survey response",
+      },
+    },
+    createdAt: { type: Date, default: Date.now() },
   },
-});
+  { timestamps: true },
+);
+
+surveyResponseSchema.index({ anonymousId: 1, survey: 1 }, { unique: true });
 
 export type ISurveyResponseSchema = InferSchemaType<typeof surveyResponseSchema>;
 
