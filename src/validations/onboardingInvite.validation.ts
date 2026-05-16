1ebc93417f429cbfdf3d456395448671c1b0bbec
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-16 18:39:24 +0530

    Rename Zod schemas to more meaningful and descriptive names

--- src/validations/onboardingInvite.validation.ts
+++ src/validations/onboardingInvite.validation.ts
@@ -5,8 +5,10 @@ export const documentIdSchema = z
   .string()
   .refine((val) => ObjectId.isValid(val), { error: "Invalid resource ID" });
 
-export const inviteSchema = z.object({ params: z.object({ employeeId: documentIdSchema }) });
-export type InviteCreationInputData = z.infer<typeof inviteSchema>;
+export const inviteCreationRequestSchema = z.object({
+  params: z.object({ employeeId: documentIdSchema }),
+});
+export type InviteCreationInputData = z.infer<typeof inviteCreationRequestSchema>;
 
-export const inviteActionSchema = z.object({ params: z.object({ inviteId: documentIdSchema }) });
-export type InviteActionInputData = z.infer<typeof inviteActionSchema>;
+export const inviteReplySchema = z.object({ params: z.object({ inviteId: documentIdSchema }) });
+export type InviteActionInputData = z.infer<typeof inviteReplySchema>;
