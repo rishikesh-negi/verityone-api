d4d2888453d91e4f0db1b367b48bf4a5ca75f20b
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-16 18:41:09 +0530

    Add endpoint to retrieve all invites received by the user (employee), and update changed Zod schema names

--- src/routes/onboardingInviteRoutes.ts
+++ src/routes/onboardingInviteRoutes.ts
@@ -3,10 +3,14 @@ import { protect, restrictTo } from "../controllers/authController.js";
 import {
   acceptInvite,
   createInvite,
+  getEmployeeInvites,
   rejectInvite,
 } from "../controllers/onboardingInviteController.js";
 import { validateRequest } from "../middleware/validateRequest.js";
-import { inviteActionSchema, inviteSchema } from "../validations/onboardingInvite.validation.js";
+import {
+  inviteReplySchema,
+  inviteCreationRequestSchema,
+} from "../validations/onboardingInvite.validation.js";
 import { BadRequestError } from "../errors/AppError.js";
 
 const router = Router();
@@ -16,21 +20,23 @@ router.use(protect);
 router.post(
   "/onboard/:employeeId",
   restrictTo("Workplace"),
-  validateRequest(inviteSchema, new BadRequestError()) as RequestHandler,
+  validateRequest(inviteCreationRequestSchema, new BadRequestError()) as RequestHandler,
   createInvite,
 );
+
+router.get("/", restrictTo("Employee"), getEmployeeInvites);
 
 router.patch(
   "/accept/:inviteId",
   restrictTo("Employee"),
-  validateRequest(inviteActionSchema, new BadRequestError()) as RequestHandler,
+  validateRequest(inviteReplySchema, new BadRequestError()) as RequestHandler,
   acceptInvite,
 );
 
 router.patch(
   "/reject/:inviteId",
   restrictTo("Employee"),
-  validateRequest(inviteActionSchema, new BadRequestError()) as RequestHandler,
+  validateRequest(inviteReplySchema, new BadRequestError()) as RequestHandler,
   rejectInvite,
 );
 
