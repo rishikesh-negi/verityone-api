bf37c98a1aa650c865a61d0070f4b60bbec0d2c2
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-27 18:39:25 +0530

    feat: Add a route for manual and early termination of a survey

--- src/routes/surveyRoutes.ts
+++ src/routes/surveyRoutes.ts
@@ -1,8 +1,12 @@
 import { Router, type RequestHandler } from "express";
 import { protect, restrictTo } from "../controllers/authController.js";
-import { createSurvey } from "../controllers/surveyController.js";
+import { createSurvey, endSurvey } from "../controllers/surveyController.js";
+import { UnprocessableContentError } from "../errors/AppError.js";
 import { validateRequest } from "../middleware/validateRequest.js";
-import { surveyCreationRequestSchema } from "../validations/survey.validation.js";
+import {
+  surveyCreationRequestSchema,
+  surveyEndRequestSchema,
+} from "../validations/survey.validation.js";
 
 const router = Router();
 
@@ -13,6 +17,15 @@ router.post(
   restrictTo("Workplace"),
   validateRequest(surveyCreationRequestSchema) as RequestHandler,
   createSurvey,
+);
+
+router.patch(
+  "/end/:surveyId",
+  validateRequest(
+    surveyEndRequestSchema,
+    new UnprocessableContentError("Survey not found"),
+  ) as RequestHandler,
+  endSurvey,
 );
 
 export default router;
