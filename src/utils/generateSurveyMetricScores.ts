f88cbf77566a1fe709b9d7e75a792b9f34d3ecdb
Rishikesh Negi <rishinegi97@gmail.com>
2026-05-28 18:04:13 +0530

    refactor: update the logic of survey metric scores calculation to match the new structure, and return the metric scores

--- src/utils/generateSurveyMetricScores.ts
+++ src/utils/generateSurveyMetricScores.ts
@@ -1,30 +1,21 @@
-import { surveyMetrics, type CruxResult } from "../data/surveyQuestions.js";
+import { surveyMetrics, type CruxScoreAndRemark } from "../data/surveyQuestions.js";
 
-const kebabToCamelCase = (str: string) =>
-  str
-    .split("-")
-    .map((word, i) => (i === 0 ? word : `${word[0]!.toUpperCase()}${word.slice(1)}`))
-    .join("");
-
-export const generateSurveyMetricScores = (cruxAverages: CruxResult[]) => {
-  const metricAverages = Object.fromEntries(
-    surveyMetrics.map((metric) => [
-      kebabToCamelCase(metric),
-      Object.values(cruxAverages)
-        .filter((cruxScore) => cruxScore.metric === metric)
-        .reduce(
-          (accum, curr, i, arr) => ({
-            metric: curr.metric,
-            score:
-              i < arr.length - 1
-                ? curr.score + accum.score
-                : (curr.score + accum.score) / arr.length,
-          }),
-          {
-            metric: "",
-            score: 0,
-          },
-        ),
-    ]),
+export const generateSurveyMetricScores = (cruxAverages: CruxScoreAndRemark[]) => {
+  const metricAverages = surveyMetrics.map((metric) =>
+    cruxAverages
+      .filter((crux) => crux.metric === metric)
+      .reduce(
+        (accum, curr, i, arr) => ({
+          metric: curr.metric,
+          score:
+            i < arr.length - 1 ? curr.score + accum.score : (curr.score + accum.score) / arr.length,
+        }),
+        {
+          metric: "",
+          score: 0,
+        },
+      ),
   );
+
+  return metricAverages;
 };
