a58a481e398934abe13fc13aaae93099fe188256
Rishikesh Negi <rishinegi97@gmail.com>
2026-06-01 17:04:20 +0530

    refactor: Add a type argument to the generic wrapper function to specify the correct request object type, and remove redundant type assertions and non-null assertions

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -4,26 +4,22 @@ import type { EmployeeDocument } from "../models/employeeModel.js";
 import { Workplace } from "../models/workplaceModel.js";
 import { catchAsyncError } from "../utils/catchAsyncError.js";
 
-export const leaveCurrentWorkplace = catchAsyncError(async (req, res, next) => {
-  if (!(req.user as EmployeeDocument).workplace)
-    return next(new BadRequestError("You are not a part of any workplace"));
+export const leaveCurrentWorkplace = catchAsyncError<EmployeeDocument>(async (req, res, next) => {
+  if (!req.user.workplace) return next(new BadRequestError("You are not a part of any workplace"));
 
-  const workplace = await Workplace.findById((req.user as EmployeeDocument).workplace!._id);
-  if (workplace?._id.toString() !== (req.user as EmployeeDocument).workplace!._id.toString())
+  const workplace = await Workplace.findById(req.user.workplace._id);
+  if (workplace?._id.toString() !== req.user.workplace._id.toString())
     return next(
       new BadRequestError("You are not a part of the workplace you've requested to leave"),
     );
 
-  if (
-    !workplace ||
-    workplace?._id.toString() === (req.user as EmployeeDocument).workplace!._id.toString()
-  )
-    (req.user as EmployeeDocument).workplace = null;
+  if (!workplace || workplace?._id.toString() === req.user.workplace._id.toString())
+    req.user.workplace = null;
 
   const session = await mongoose.startSession();
   session.startTransaction();
   try {
-    const updatedUser = await (req.user as EmployeeDocument).save({ session });
+    const updatedUser = await req.user.save({ session });
     await workplace.updateOne({ $inc: { numEmployees: -1 } });
     await session.commitTransaction();
     return res.status(200).json({
