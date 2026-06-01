588779990e4967f3f1ab797d9d34f056e116b580
Rishikesh Negi <rishinegi97@gmail.com>
2026-06-01 16:51:28 +0530

    refactor: Refactor wrapper function to be generic and dynamically determine the Request type in the returned route handler

--- src/utils/catchAsyncError.ts
+++ src/utils/catchAsyncError.ts
@@ -1,8 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
-import type { AsyncRouteHandler } from "../types/types.js";
+import type { AsyncRouteHandler, RequestWithUser, UserDocument } from "../types/types.js";
 
-export function catchAsyncError(fn: AsyncRouteHandler) {
-  return function (req: Request, res: Response, next: NextFunction) {
+export function catchAsyncError<T extends UserDocument | undefined = undefined>(
+  fn: AsyncRouteHandler<T>,
+) {
+  return function (
+    req: T extends UserDocument ? RequestWithUser<T> : Request,
+    res: Response,
+    next: NextFunction,
+  ) {
     fn(req, res, next).catch((err) => next(err));
   };
 }
